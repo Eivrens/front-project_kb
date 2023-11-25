8b9470752e21db456682fba21e087658664c66e3
Jean Carlos <jeancarlos.techdev@gmail.com>
2023-11-25 02:07:04 -0300

    RF001: Construção do header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
 import "./Header.css";
 import logo from "../../assets/icons/logo.png";
+import menu from "../../assets/icons/icons8-cardápio-96.png";
 
 export default function Header() {
   return (
-    <header className="header">
+    <div className="header">
       <button className="btn-home" type="button">
         <img className="img-home" src={logo} alt="Tela inicial" />
       </button>
-    </header>
+      {/* transformar em componente */}
+      <div className="switch-button">
+        <div className="active-switch">
+          <p>Adote um gato</p>
+        </div>
+        <div className="disable-switch">
+          <p>Apoie uma causa</p>
+        </div>
+      </div>
+      {/* transformar em componente */}
+      <button className="btn-menu">
+        <img src={menu} alt="Tela inicial" />
+      </button>
+    </div>
   );
 }

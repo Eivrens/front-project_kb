daca2ca909830ac4618198bf7f16bac590830111
Jean Carlos <jeancarlos.techdev@gmail.com>
2023-11-30 18:20:11 -0300

    RFF001: Header com a adição dos componentes

---
+++ src/components/SwitchButton/SwitchButton.js
@@ -0,0 +1,22 @@
+import './SwitchButton.css';
+import React, { useState } from 'react';
+
+function SwitchButton({  }) {
+
+  const [active, setActive] = useState(false);
+
+  const onClickHandler = (e) => {
+    setActive(!active);
+    
+  };
+
+  return (
+    <div className="switch-button" onClick={onClickHandler}>
+      <div style={{ left: active ? '50%' : '0' }}></div>
+      <div>Adote um gato</div>
+      <div>Apoie uma causa</div>
+    </div >
+  )
+}
+
+export default SwitchButton;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,14 @@
 import Header from "../../components/Header/Header";
 
+import "./Home.css";
+
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <div className="home">
+      <div className="header-home">
+        <Header />
+      </div>
+      <div className="body-home"></div>
     </div>
-  )
+  );
 }

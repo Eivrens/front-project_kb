6aa8ce0cec3c7f5c9387bd32db1efb8f8ddbb67a
MatheusSantos537 <matheusgabrieldossantos4@gmail.com>
2023-11-30 23:43:19 -0300

    RFF004:Criação da aba lateral-Hamburguer in-progress

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,25 @@
 import './Menu.css'
 import menu from "../../assets/icons/icons8-cardápio-96.png";
+import React from 'react';
+import FloatMenu from '../FloatMenu/FloatMenu'; // Corrija o nome do arquivo de importação
+import { useState } from 'react';
 
-export default function Menu() {
+ function Menu(e) {
+  const [openMenu, setOpenMenu] = useState(false);
+  //e.eventListener('click',)
+  
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
-    <button className="btn-menu">
-      <img src={menu} alt="Tela inicial" />
-    </button>
-  )
+    <div className="btn-menu">
+      <button className="btn-menu" onClick={toggleMenu}>
+        <img src={menu} alt="Tela inicial" />
+      </button>
+      {openMenu && <FloatMenu onClose={toggleMenu} />}
+    </div>
+  );
 }
+
+export default Menu

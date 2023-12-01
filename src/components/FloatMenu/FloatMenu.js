2d0214dff358c94004ba46941fd6a9bfbdc2e979
MatheusSantos537 <matheusgabrieldossantos4@gmail.com>
2023-12-01 13:03:34 -0300

    RFF004:Criação da aba lateral-Hamburguer in-progress

--- src/components/FloatMenu/FloatMenu.js
+++ src/components/FloatMenu/FloatMenu.js
@@ -1,20 +1,30 @@
-import React from 'react'; // Esta linha parece estar duplicada
+import React, { useEffect, useRef } from 'react';
 import './FloatMenu.css';
-import login from '../../assets/icons/login96.png';
-//E:\Project_KB\front-project_kb\src\assets\icons\icons8-login-arredondado-a-direita-96.png
-function FloatMenu(e) {
-  //on click for negativo se n for na div do hamburguer
- 
-  
-  return (
-    <div className='floatMenu'>
-      <div>
+import login from '../../assets/icons/login-96.png';
 
-      
-     <a><img src={login}/>LOGIN</a>
+function FloatMenu({ onClose }) {
+
+  return (
+    <div className='floatMenu' >
+      <div className='floatMenuContainer'>
+        <div className='floatContainer'>
+          <a>
+            <img src={login} alt='Login Icon' />
+            <span>Login</span>
+          </a>
+          <hr />
+        </div>
+        {/* <div className='floatContainer'>
+          <a>
+            <img src={login} alt='Login Icon' />
+            <span>Login</span>
+          </a>
+          <hr />
+        </div> */}
+
       </div>
     </div>
   );
 }
 
-export default FloatMenu; // Aqui, corrija para exportar
+export default FloatMenu;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,24 @@
 import './Menu.css'
 import menu from "../../assets/icons/icons8-cardápio-96.png";
 import React from 'react';
-import FloatMenu from '../FloatMenu/FloatMenu'; // Corrija o nome do arquivo de importação
+import FloatMenu from '../FloatMenu/FloatMenu.js'; 
 import { useState } from 'react';
 
  function Menu(e) {
   const [openMenu, setOpenMenu] = useState(false);
-  //e.eventListener('click',)
+  
   
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
   };
 
   return (
+    <div>  
     <div className="btn-menu">
       <button className="btn-menu" onClick={toggleMenu}>
         <img src={menu} alt="Tela inicial" />
       </button>
+    </div>
       {openMenu && <FloatMenu onClose={toggleMenu} />}
     </div>
   );

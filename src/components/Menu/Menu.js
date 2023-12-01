372273af7e34969a64788688d9f0f5490946ecb5
MatheusSantos537 <matheusgabrieldossantos4@gmail.com>
2023-12-01 15:20:39 -0300

    RFF004:Criação da aba lateral-Hamburguer read-to-test

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,20 +2,45 @@ import './Menu.css'
 import menu from "../../assets/icons/icons8-cardápio-96.png";
 import React from 'react';
 import FloatMenu from '../FloatMenu/FloatMenu.js'; 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
- function Menu(e) {
+ function Menu() {
   const [openMenu, setOpenMenu] = useState(false);
-  
+  const menuButtonRef = useRef(null);
   
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
   };
 
+  const getDocumentClick=(e)=>{
+    
+    if(menuButtonRef.current &&menuButtonRef.current.contains(e.target)){
+      return;
+    }
+   
+    const clickInside=e.target.closest('.floatMenu');
+    if(clickInside){
+      return;
+    }
+    setOpenMenu(false);
+  }
+
+  
+
+  useEffect(()=>{
+    document.addEventListener('click',getDocumentClick)
+
+    return ()=>{
+      document.removeEventListener('click',getDocumentClick)
+    };
+  },[])
+
+
+
   return (
     <div>  
     <div className="btn-menu">
-      <button className="btn-menu" onClick={toggleMenu}>
+      <button  ref={menuButtonRef} className="btn-menu" onClick={toggleMenu}>
         <img src={menu} alt="Tela inicial" />
       </button>
     </div>
